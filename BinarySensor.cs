130fd1d5802c5ccdd20f0ad936a8a0b22716a51d
Rune de Groot <rune.de.groot@ovsoftware.com>
2022-09-26 19:23:25 +0200

    Initial commit

---
+++ BinarySensor.cs
@@ -0,0 +1,11 @@
+﻿namespace MyUpwayMQTTBridge
+{
+    public readonly record struct BinarySensor(
+        string Name,
+        int ID,
+        string Identifier,
+        string DeviceClass,
+        string PayloadOn,
+        string PayloadOff
+    );
+}
